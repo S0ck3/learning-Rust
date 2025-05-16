5e7ffff0a7eb2203c0ef2055b5943d1bd3af40ba
S0ck3 <antonia.gentsch@gmail.com>
2025-05-16 12:31:22 +0200

    changed project naming
    projects now got numbers for better structure

--- 009_letter_counting/src/main.rs
+++ 009_letter_counting/src/main.rs
@@ -1,7 +1,6 @@
 use std::io; // standard library for input/output
 
 fn main() {
-
     println!("Please enter a word: ");
 
     let mut input = String::new(); // new string for the input
@@ -11,19 +10,19 @@ fn main() {
 
     let mut v = 0;
     let mut c = 0;
-    let vowels = vec!['a','e','i','o','u','A','E','I','O','U']; // define vowels in vector
-        
+    let vowels = vec!['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']; // define vowels in vector
+
     for char in input.chars() {
-        
         if char.is_alphabetic() {
-            if vowels.contains(&char) {    // checks if char is defined in vector "vowels"
-                v += 1;                         // if yes, the vowel count is increased by 1
-            } else { 
-                c += 1;                         // if no, the consonant count is increased by 1
-            } 
-        }       
+            if vowels.contains(&char) {
+                // checks if char is defined in vector "vowels"
+                v += 1; // if yes, the vowel count is increased by 1
+            } else {
+                c += 1; // if no, the consonant count is increased by 1
+            }
+        }
     }
-    
+
     println!("Vowel count: {}", v);
     println!("Consonant count: {}", c);
 }
